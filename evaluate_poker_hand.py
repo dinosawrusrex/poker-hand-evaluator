446ce56b107551e2b5a42e4d98be02fa24a67105
dinosawrusrex <dennissaw12@gmail.com>
2018-05-24 15:34:08 -0700

    Poker hand evaluator

--- evaluate_poker_hand.py
+++ evaluate_poker_hand.py
@@ -1,18 +1,38 @@
 # Conversions
-ROYALTY_CONVERSIONS = {'j': 10, 'q': 11, 'k': 12}
+ROYALTY_CONVERSIONS = {'jack': 11, 'queen': 12, 'king': 13}
 
-def ACE_CONVERSION(ranks):
+def ace_conversion(ranks):
     low_order = {2,3,4,5}
     if set(ranks).intersection(low_order) == low_order:
         return(1)
     else:
-        return(13)
+        return(14)
 
 def convert_alpha(rank, ranks):
-    if rank == 'a':
-        return(ACE_CONVERSION(ranks))
+    if rank == 'ace':
+        return(ace_conversion(ranks))
     else:
         return(ROYALTY_CONVERSIONS[rank])
+
+def turn_ranks_to_numbers(hand):
+    ranks = [int(card[1]) if card[1].isdigit() else card[1] for card in hand]
+    ranks = sorted([convert_alpha(rank, ranks) if isinstance(rank, str)
+                    else rank for rank in ranks])
+    return(ranks)
+
+def separate_ranks_and_suits(hand):
+    return([card.split(' ') for card in hand])
+
+# Ranking
+SCORE = {'high card': 1,
+           'one pair': 2,
+           'two pair': 3,
+           'three of a kind': 4,
+           'straight': 5,
+           'flush': 6,
+           'full house': 7,
+           'four of a kind': 8,
+           'straight flush': 9}
 
 # Hands
 with open('hands', 'r') as hands:
@@ -21,10 +41,13 @@ with open('hands', 'r') as hands:
 hand_one, hand_two = [sorted(hand.split(', ')) for hand in hands.split('\n')
                       if hand != '']
 
-trial_hand = ['sa', 's2', 's6', 's4', 's5']
+hand_one = separate_ranks_and_suits(hand_one)
+hand_two = separate_ranks_and_suits(hand_two)
 
+trial_hand = ['spade ace', 'diamond 2', 'spade 3', 'club 3', 'heart 5']
+trial_hand = separate_ranks_and_suits(trial_hand)
 
-# Check for flush and straights
+# For checking flush and straights
 def check_for_same_suit(hand):
     suits = [card[0] for card in hand]
     if len(set(suits)) == 1:
@@ -33,16 +56,19 @@ def check_for_same_suit(hand):
         return(False)
 
 def check_for_numeric_order(hand):
-    ranks = [int(card[1]) if card[1].isdigit() else card[1] for card in hand]
-    ranks = sorted([convert_alpha(rank, ranks) if isinstance(rank, str)
-                    else rank for rank in ranks])
+    ranks = turn_ranks_to_numbers(hand)
     output = sum([ranks[i]-ranks[i+1] for i in range(len(ranks)-1)]) == -4
     return(output)
 
+
 # Check for identical ranks
+def create_rank_patterns(hand):
+    ranks = turn_ranks_to_numbers(hand)
+    count_pattern = sorted([ranks.count(rank) for rank in set(ranks)])
+    return(count_pattern)
 
 
-
+# Identify poker hand
 def rank_hand(hand):
     if check_for_numeric_order(hand):
         if check_for_same_suit(hand):
@@ -50,5 +76,33 @@ def rank_hand(hand):
         return('straight')
     elif check_for_same_suit(hand) and not check_for_numeric_order(hand):
         return('flush')
+    elif create_rank_patterns(hand) == [1,4]:
+        return('four of a kind')
+    elif create_rank_patterns(hand) == [2,3]:
+        return('full house')
+    elif create_rank_patterns(hand) == [1,1,3]:
+        return('three of a kind')
+    elif create_rank_patterns(hand) == [1,2,2]:
+        return('two pair')
+    elif create_rank_patterns(hand) == [1,1,1,2]:
+        return('one pair')
+    else:
+        return('high card')
 
-print(rank_hand(trial_hand))
+def evaluate_hands(hand_one, hand_two):
+    if SCORE[rank_hand(hand_one)] > SCORE[rank_hand(hand_two)]:
+        return('hand one wins 1')
+    elif SCORE[rank_hand(hand_one)] < SCORE[rank_hand(hand_two)]:
+        return('hand two wins 1')
+    else: # If both hands have the same 'points'
+        if max(turn_ranks_to_numbers(hand_one)) > \
+           max(turn_ranks_to_numbers(hand_two)):
+            return('hand one wins 2')
+        elif max(turn_ranks_to_numbers(hand_one)) < \
+           max(turn_ranks_to_numbers(hand_two)):
+            return('hand two wins 2')
+        else:
+            return('identical hands')
+
+print(hand_one, '\n', hand_two)
+print(evaluate_hands(hand_one, hand_two))
