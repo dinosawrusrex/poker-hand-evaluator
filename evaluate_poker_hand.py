62695e6b74e0300041a3a96eb0f0bef0075d967d
dinosawrusrex <dennissaw12@gmail.com>
2018-05-24 13:20:47 -0700

    WIP

--- evaluate_poker_hand.py
+++ evaluate_poker_hand.py
@@ -1,13 +1,30 @@
+# Conversions
 ROYALTY_CONVERSIONS = {'j': 10, 'q': 11, 'k': 12}
 
-#ACE_CONVERSION
+def ACE_CONVERSION(ranks):
+    low_order = {2,3,4,5}
+    if set(ranks).intersection(low_order) == low_order:
+        return(1)
+    else:
+        return(13)
 
+def convert_alpha(rank, ranks):
+    if rank == 'a':
+        return(ACE_CONVERSION(ranks))
+    else:
+        return(ROYALTY_CONVERSIONS[rank])
+
+# Hands
 with open('hands', 'r') as hands:
     hands = hands.read()
 
 hand_one, hand_two = [sorted(hand.split(', ')) for hand in hands.split('\n')
                       if hand != '']
 
+trial_hand = ['sa', 's2', 's6', 's4', 's5']
+
+
+# Check for flush and straights
 def check_for_same_suit(hand):
     suits = [card[0] for card in hand]
     if len(set(suits)) == 1:
@@ -17,16 +34,21 @@ def check_for_same_suit(hand):
 
 def check_for_numeric_order(hand):
     ranks = [int(card[1]) if card[1].isdigit() else card[1] for card in hand]
-    if 'a' in ranks:
-        ranks = ranks.remove('a')
-    return(ranks)
+    ranks = sorted([convert_alpha(rank, ranks) if isinstance(rank, str)
+                    else rank for rank in ranks])
+    output = sum([ranks[i]-ranks[i+1] for i in range(len(ranks)-1)]) == -4
+    return(output)
 
-print(hand_two)
-print(check_for_numeric_order(hand_two))
+# Check for identical ranks
+
 
 
 def rank_hand(hand):
-    for card in hand:
-        if hand[0][0] in card:
-            continue
+    if check_for_numeric_order(hand):
+        if check_for_same_suit(hand):
+            return('straight flush')
+        return('straight')
+    elif check_for_same_suit(hand) and not check_for_numeric_order(hand):
+        return('flush')
 
+print(rank_hand(trial_hand))
