23ab294ff9179c2de519c77b1b2b0db300a7c6be
dinosawrusrex <dennissaw12@gmail.com>
2018-05-30 23:52:17 -0700

    WIP

--- evaluate_poker_hand.py
+++ evaluate_poker_hand.py
@@ -32,24 +32,8 @@ class Card:
 
 
 class Hand:
-    def __init__(self, hand):
-        self.cards = [Card(hand[card], hand) for card in hand]
 
-class Game:
-    def __init__(self, data):
-        self.hand_one = Hand(data['hand_one'])
-        self.hand_two = Hand(data['hand_two'])
-
-with open('hands.json', 'r') as hands:
-    data = json.load(hands)
-
-#print(data['hand_one']['one'])
-game = Game(data)
-print(game.hand_two.cards[1].rank_score)
-
-
-# Ranking
-SCORE = {'high card': 1,
+    SCORE = {'high card': 1,
          'one pair': 2,
          'two pair': 3,
          'three of a kind': 4,
@@ -59,83 +43,84 @@ SCORE = {'high card': 1,
          'four of a kind': 8,
          'straight flush': 9}
 
+    def __init__(self, hand):
+        self.cards = [Card(hand[card], hand) for card in hand]
+        self.poker_hand = self.rank_hand()
+        self.score = self.SCORE[self.poker_hand]
 
-# For checking flush and straights
-def check_for_same_suit(hand):
-    suits = [card[0] for card in hand]
-    if len(set(suits)) == 1:
-        return(True)
-    else:
-        return(False)
-
-def check_for_numeric_order(hand):
-    ranks = turn_ranks_to_numbers(hand)
-    output = sum([ranks[i]-ranks[i+1] for i in range(len(ranks)-1)]) == -4
-    return(output)
-
-
-# Check for identical ranks
-def create_rank_patterns(hand):
-    ranks = turn_ranks_to_numbers(hand)
-    count_pattern = sorted([ranks.count(rank) for rank in set(ranks)])
-    return(count_pattern)
-
-
-# Identify poker hand
-def rank_hand(hand):
-    if check_for_numeric_order(hand):
-        if check_for_same_suit(hand):
-            return('straight flush')
-        return('straight')
-    elif check_for_same_suit(hand) and not check_for_numeric_order(hand):
-        return('flush')
-    elif create_rank_patterns(hand) == [1,4]:
-        return('four of a kind')
-    elif create_rank_patterns(hand) == [2,3]:
-        return('full house')
-    elif create_rank_patterns(hand) == [1,1,3]:
-        return('three of a kind')
-    elif create_rank_patterns(hand) == [1,2,2]:
-        return('two pair')
-    elif create_rank_patterns(hand) == [1,1,1,2]:
-        return('one pair')
-    else:
-        return('high card')
-
-
-# Compare two hands
-def mode_rank(hand):
-    ranks = turn_ranks_to_numbers(hand)
-    output = max(set(ranks), key=ranks.count)
-    return(output)
-
-def evaluate_hands(hand_one, hand_two):
-    if SCORE[rank_hand(hand_one)] > SCORE[rank_hand(hand_two)]:
-        return('hand one wins by score')
-    elif SCORE[rank_hand(hand_one)] < SCORE[rank_hand(hand_two)]:
-        return('hand two wins by score')
-    else: # If both hands have the same 'points'
-        if mode_rank(hand_one) > mode_rank(hand_two):
-            return('hand one wins by max rank')
-        elif mode_rank(hand_one) < mode_rank(hand_two):
-            return('hand two wins by max rank')
+    def check_for_flush(self):
+        suits = [card.suit for card in self.cards]
+        if len(set(suits)) == 1:
+            return(True)
         else:
-            return('identical hands')
-'''
-# Run comparison
-hand_one, hand_two = [sorted(hand.split(', ')) for hand in hands.split('\n')
-                      if hand != '']
+            return(False)
 
-hand_one = separate_ranks_and_suits(hand_one)
-hand_two = separate_ranks_and_suits(hand_two)
+    def check_for_straight(self):
+        ranks = sorted([card.rank_score for card in self.cards])
+        output = [ranks[i]-ranks[i+1] for i in range(len(ranks)-1)].count(-1)\
+                == 4
+        return(output)
 
-hands_vs = evaluate_hands(hand_one, hand_two)
+    def create_rank_pattern(self):
+        ranks = [card.rank for card in self.cards]
+        count_pattern = sorted([ranks.count(rank) for rank in set(ranks)])
+        return(count_pattern)
 
-print('Hand one has {}, which is a {} and has a score of {}.'.format(
-    hand_one, rank_hand(hand_one), SCORE[rank_hand(hand_one)]))
-print()
-print('Hand two has {}, which is a {} and has a score of {}.'.format(
-    hand_two, rank_hand(hand_two), SCORE[rank_hand(hand_two)]))
-print()
-print(hands_vs)
-'''
+    def rank_hand(self):
+        if self.check_for_straight():
+            if self.check_for_flush():
+                return('straight flush')
+            return('straight')
+        elif self.check_for_flush() and not self.check_for_straight():
+            return('flush')
+        elif self.create_rank_pattern() == [1,4]:
+            return('four of a kind')
+        elif self.create_rank_pattern() == [2,3]:
+            return('full house')
+        elif self.create_rank_pattern() == [1,1,3]:
+            return('three of a kind')
+        elif self.create_rank_pattern() == [1,2,2]:
+            return('two pair')
+        elif self.create_rank_pattern() == [1,1,1,2]:
+            return('one pair')
+        else:
+            return('high card')
+
+
+class Game:
+    def __init__(self, data):
+        self.hand_one = Hand(data['hand_one'])
+        self.hand_two = Hand(data['hand_two'])
+
+    def index_to_compare(self):
+        scores_one = [card.rank_score for card in self.hand_one.cards]
+        scores_two = [card.rank_score for card in self.hand_two.cards]
+        scores_one = sorted(set(scores_one), key=scores_one.count)
+        scores_two = sorted(set(scores_two), key=scores_two.count)
+        index = len(scores_one)-1
+        for i in range(len(scores_one)-1, -1, -1):
+            index = i
+            print(index)
+            if scores_one[i] != scores_two[i]:
+                return(index)
+        return(index)
+
+    def evaluate_hands(self):
+        if self.hand_one.score > self.hand_two.score:
+            return('hand one wins by score')
+        elif self.hand_one.score < self.hand_two.score:
+            return('hand two wins by score')
+        else: # If both hands have the same 'points'
+            index = self.index_to_compare()
+            if self.hand_one.cards[index].rank_score > self.hand_two.cards[index].rank_score:
+                return('hand one wins by max rank_score')
+            elif self.hand_one.cards[index].rank_score < self.hand_two.cards[index].rank_score:
+                return('hand two wins by max rank_score')
+            else:
+                return('identical hands')
+
+with open('hands.json', 'r') as hands:
+    data = json.load(hands)
+
+game = Game(data)
+print(game.evaluate_hands())
