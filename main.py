b7ab7f3e63a1b5fa4a7ba7b5ccc2bf6c67823857
dinosawrusrex <dennissaw12@gmail.com>
2018-06-19 16:16:43 -0700

    Revert "Update file name import and function name"
    This reverts commit be8c851ae29323b2a46147c9b63098e180d7018c.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import json, poker_hand_evaluator, random_hand_generator
+import json, evaluate_poker_hand, random_hand_generator
 
 if __name__ == '__main__':
     with open('card_template.json', 'r+') as hands:
         template = json.load(hands)
-        new_hands = random_hand_generator.generate_random_hand(template)
-    game = poker_hand_evaluator.Game(new_hands)
+        new_hands = random_hand_generator.generate_random_card(template)
+    game = evaluate_poker_hand.Game(new_hands)
     game.print_information()
 
